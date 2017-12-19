7ecc3162c104baaf39e1acb1563c5d6a58fa6376
Maruthi Mohan Reddy Putha <maruthim@buffalo.edu>
2017-12-18 19:32:11 -0500

    Commit a test java file

---
+++ src/Algorithms/NewTest.java
@@ -0,0 +1,10 @@
+public class PrintMyName{
+    String myName;
+    public PrintMyName(String s){
+        this.myName = s;
+    }
+
+    public static void main(String[] args){
+        System.out.println("My name is "+myName);
+    }
+}
